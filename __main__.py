487bb2617fab081a5769175f682f52ece3043a72
Zhenzhong Tang <ztang@berkeley.edu>
2019-11-26 06:08:53 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -8,7 +8,7 @@ edge_points = edge_detect(face_crop(img))
 path_index = dfs(mst(adj_matrix(edge_points)))
 path = edge_points[path_index]
 
+print("len of original path: {}".format(len(path)))
 for eps in [0.5, 1, 1.5, 2, 3, 5, 10]:
-    print("eplison = {}".format(eps))
     downsample_path = rdp(path, epsilon=eps)
-    print("len of path: {} \t len of downsampled path: {}".format(len(path), len(downsample_path)))
+    print("eplison = {} \t len of downsampled path: {}".format(eps, len(downsample_path)))
