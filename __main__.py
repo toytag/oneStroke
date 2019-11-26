27c1b3395ed90d102459ee0800bcb564a99741c9
Zhenzhong Tang <ztang@berkeley.edu>
2019-11-26 07:27:10 +0000

    Ready for deploy

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import sys, cv2
+import matplotlib.pyplot as plt
 from oneStroke import *
 
 img_path = sys.argv[1]
@@ -12,3 +13,9 @@ print("len of original path: {}".format(len(path)))
 for eps in [0.5, 1, 1.5, 2, 2.56, 3, 5, 10]:
     downsample_path = rdp(path, epsilon=eps)
     print("eplison = {} \t len of downsampled path: {}".format(eps, len(downsample_path)))
+
+x, y = rdp(path).T
+plt.axis("equal")
+plt.plot(x, y, c='tab:blue')
+plt.savefig("oneStroke.png")
+print("--- picture \"oneStroke.png\" saved ---")
